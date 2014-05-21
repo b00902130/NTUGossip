6c068fafb52c043128d19d0de0008fba1b57457c
b00902130 <b00902130@ntu.edu.tw>
2014-05-22 00:53:51 +0800

    some change.

--- web.js
+++ web.js
@@ -1,13 +1,19 @@
 // web.js
 var express = require("express");
 var logfmt = require("logfmt");
+var index = require("./routes/index")
 var app = express();
+
+app.set('view engine', 'jade');
+app.set('views', __dirname+"/views" );
 
 app.use(logfmt.requestLogger());
 
 app.get('/', function(req, res) {
-  res.send('Hello World!');
+  res.render('index',{title: "I love big tongue!"});
 });
+
+
 
 var port = Number(process.env.PORT || 5000);
 app.listen(port, function() {
